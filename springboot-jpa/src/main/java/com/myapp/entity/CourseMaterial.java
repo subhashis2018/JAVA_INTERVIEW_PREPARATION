04afc66829224c3da879a4c2e18db2596829d819
SUBHASHIS <subhashis.sarangi@gmail.com>
2024-02-17 07:58:49 +0530

    commit

--- springboot-jpa/src/main/java/com/myapp/entity/CourseMaterial.java
+++ springboot-jpa/src/main/java/com/myapp/entity/CourseMaterial.java
@@ -2,7 +2,12 @@ package com.myapp.entity;
 
 import java.net.URL;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -20,7 +25,10 @@ import lombok.ToString;
 @Entity
 @Table(name = "tbl_course_material")
 public class CourseMaterial {
-
+	@Id
+	@SequenceGenerator(name = "course_material_sequence", sequenceName = "course_material_sequence", allocationSize = 1)
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "course_material_sequence")
+	@Column(name = "courseMaterialId")
 	private Long courseMterialId;
 	private URL url;
 
--- springboot-jpa/src/main/java/com/myapp/entity/Teacher.java
+++ springboot-jpa/src/main/java/com/myapp/entity/Teacher.java
@@ -1,6 +1,11 @@
 package com.myapp.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -18,7 +23,10 @@ import lombok.ToString;
 @Entity
 @Table(name = "tbl_teacher")
 public class Teacher {
-	
+	@Id
+	@SequenceGenerator(name = "teacher_sequence", sequenceName = "teacher_sequence", allocationSize = 1)
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "teacher_sequence")
+	@Column(name = "teacherId")
 	private Long teacherId;
 	private String firstName;
 	private String lastName;
